72e4e8dcedf86454564c185d9edff826c0da9d5c
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2023-10-20 06:39:31 +0300

    refactor: type definitions (#35)
    * החזרת בוליאני מdeleteCall האם השיחה היתה קיימת או לא
    
    * type definitions: typo
    
    * lowercase types
    
    https://www.typescriptlang.org/docs/handbook/basic-types.html#about-number-string-boolean-symbol-and-object
    
    * fix: read type definitions
    
    * fix: router type definitions
    
    * fix router.events type
    
    * fix: Call type
    
    * fix: errors type definitions
    
    * Update jsconfig.json
    
    * type fixes
    
    * types fix
    
    * index.d.ts: code style
    
    * tpyes: fix YemotRouter
    
    implement  pure interface instead of extends Omit of express Router
    
    * type fix
    
    * router bugfix
    
    * הוספת תיעוד JSDoc של ברירות המחדל
    
    על פי lib/defaults.js
    
    * fix
    
    * add router.asExpressRouter for typescript users
    
    * type fix
    
    * add Api** types

--- index.d.ts
+++ index.d.ts
@@ -1,121 +1,227 @@
-import { EventEmitter } from 'events';
-export declare function YemotRouter(options?: { timeout: Number; printLog: Boolean; uncaughtErrorHandler: function }): YemotRouter;
+import { type EventEmitter } from 'events';
+import { type Router, type Request, type Send } from 'express';
 
-type CallHandler = (p: Call) => void;
-interface YemotRouter {
-    get: (path: String, handler: CallHandler) => void;
-    post: (path: String, handler: CallHandler) => void;
-    all: (path: String, handler: CallHandler) => void;
-    deleteCallL: (callId: String) => void;
-    events: EventEmitter;
-    defaults: {
-        printLog?: Boolean;
-        read?: {
-            timeout?: Number;
-            tap?: TapOps;
-            stt?: SstOps;
-            record?: RecordOps;
-        };
-        id_list_message?: idListMessageOptions;
+interface Defaults {
+    printLog?: boolean
+    read?: {
+        timeout?: number
+        tap?: TapOptions
+        stt?: SstOptions
+        record?: RecordOptions
     }
+    id_list_message?: IdListMessageOptions
 }
 
-export type Call = {
-    did: String;
-    phone: String;
-    real_did: String;
-    callId: String;
-    extension: String;
+// from @types/express - https://github.com/DefinitelyTyped/DefinitelyTyped/blob/f800de4ffd291820a9e444e6b6cd3ac9b4a16e53/types/express/index.d.ts#L73-#L92
+interface ExpressRouterOptions {
+    /**
+     * Enable case sensitivity.
+     */
+    caseSensitive?: boolean | undefined
 
-    read(messages: Msg[], mode?: 'tap' | 'stt' | 'record', options?: TapOps | RecordOps | SstOps): Promise<String | false>;
-    go_to_folder(target: String): void;
-    id_list_message(messages: Msg[], options?: idListMessageOptions): void;
-    routing_yemot(number: String): void;
-    restart_ext(): void;
-    hangup(): void;
-};
+    /**
+     * Preserve the req.params values from the parent router.
+     * If the parent and the child have conflicting param names, the child’s value take precedence.
+     *
+     * @default false
+     * @since 4.5.0
+     */
+    mergeParams?: boolean | undefined
 
-type Msg = {
-    type: 'file' | 'text' | 'speech' | 'digits' | 'number' | 'alpha' | 'zmanim' | 'go_to_folder' | 'system_message' | 'music_on_hold' | 'date' | 'dateH';
-    data:
-        | String
-        | Number
-        | {
-              time?: String;
-              zone?: String;
-              difference?: String;
-          };
-    removeInvalidChars?: Boolean;
-};
-
-type GeneralOps = {
-    val_name?: String;
-    re_enter_if_exists?: Boolean;
-    removeInvalidChars?: Boolean;
-};
-
-type TapOps = GeneralTapOps & {
-    max_digits?: Number;
-    min_digits?: Number;
-    sec_wait?: Number;
-    typing_playback_mode?: 'Number' | 'Digits' | 'File' | 'TTS' | 'Alpha' | 'No' | 'HebrewKeyboard' | 'EmailKeyboard' | 'EnglishKeyboard' | 'DigitsKeyboard' | 'TeudatZehut' | 'Price' | 'Time' | 'Phone' | 'No';
-    block_asterisk_key?: Boolean;
-    block_zero_key?: Boolean;
-    replace_char?: String;
-    digits_allowed?: Array<Number | String>;
-    amount_attempts?: Number;
-    allow_empty?: Boolean;
-    empty_val: String;
-    block_change_type_lang: Boolean;
-};
-
-type SstOps = GeneralTapOps & {
-    lang: String;
-    block_typing?: Boolean;
-    max_digits?: Number;
-    use_records_recognition_engine?: Boolean;
-    quiet_max?: Number;
-    length_max?: Number;
-};
-
-type RecordOps = GeneralTapOps & {
-    path: String;
-    file_name: String;
-    no_confirm_menu: Boolean;
-    save_on_hangup: Boolean;
-    append_to_existing_file: Boolean;
-    min_length?: Number;
-    max_length?: Number;
-};
-
-type idListMessageOptions = {
-    removeInvalidChars?: Boolean;
-    prependToNextAction?: Boolean;
-};
-
-class CallError extends Error {
-    name: String;
-    message: String;
-    callerFile: String;
-    call: Call;
-    date: Date;
-    isYemotRouterError: Boolean;
-    constructor ({ message, call = null }) {
-    }
+    /**
+     * Enable strict routing.
+     */
+    strict?: boolean | undefined
 }
 
-class ExitError extends CallError {
-    constructor(call: Call, context: Object) {}
-}
-
-class HangupError extends CallError { }
-
-class TimeoutError extends CallError {
-    constructor(call: Call) { }
-}
-
-export const errors = {
-    ExitError,
-    HangupError,
-    TimeoutError
+type YemotRouterOptions = ExpressRouterOptions & {
+    timeout?: number
+    printLog?: boolean
+    uncaughtErrorHandler?: (error: Error, call: Call) => void | Promise<unknown>
+    defaults?: Defaults
 };
+
+type CallHandler = (call: Call) => Promise<unknown>;
+
+interface RouterEventEmitter extends EventEmitter {
+    on: (eventName: 'call_hangup' | 'call_continue' | 'new_call', listener: (call: Call) => void) => this
+    once: (eventName: 'call_hangup' | 'call_continue' | 'new_call', listener: (call: Call) => void) => this
+}
+
+export function YemotRouter (options?: YemotRouterOptions): {
+    get: (path: string, handler: CallHandler) => void
+    post: (path: string, handler: CallHandler) => void
+    all: (path: string, handler: CallHandler) => void
+    use: Router['use']
+    /**
+     * delete call from active calls by callId
+     * @returns true if the call was deleted, false if the call was not found
+     */
+    deleteCall: (callId: string) => boolean
+    events: RouterEventEmitter
+    defaults: Defaults
+    /**
+     * מתודה לשימוש בטייפסקריפט בלבד
+     * ---------------
+     * מחזיר את הראוטר ‫(`YemotRouter`) כפי שהוא - עם טייפ של ‫`Express.Router`, למניעת שגיאת טייפ
+     * @example ```ts
+     * import express from 'express';
+     * import { YemotRouter } from 'yemot-router';
+     *  
+     * const app = express();
+     * const router = YemotRouter();
+     * 
+     * app.use(router.asExpressRouter()); // 👈👈👈
+     * ```
+     */
+    asExpressRouter: () => Router
+};
+
+// based of https://tchumim.com/post/157692, https://tchumim.com/post/157706
+interface ReadModes {
+    tap: TapOptions
+    stt: SstOptions
+    record: RecordOptions
+}
+
+export interface Call {
+    read: <T extends keyof ReadModes>(messages: Msg[], mode: T, options?: ReadModes[T]) => Promise<string>
+    go_to_folder: (target: string) => void
+    id_list_message: (messages: Msg[], options?: IdListMessageOptions) => void
+    routing_yemot: (number: string) => void
+    restart_ext: () => void
+    hangup: () => void
+    send: Send
+    values: Readonly<Record<string, string>>
+    defaults: Defaults
+    req: Request
+    did: string
+    phone: string
+    real_did: string
+    callId: string
+    extension: string
+    ApiCallId: string
+    ApiPhone: string
+    ApiDID: string
+    ApiRealDID: string
+    ApiExtension: string
+    ApiEnterID: string
+    ApiEnterIDName: string
+    ApiTime: string
+    ApiYFCallId: string
+}
+
+export interface Msg {
+    type: 'file' | 'text' | 'speech' | 'digits' | 'number' | 'alpha' | 'zmanim' | 'go_to_folder' | 'system_message' | 'music_on_hold' | 'date' | 'dateH'
+    data: string | number | { time?: string, zone?: string, difference?: string }
+    removeInvalidChars?: boolean
+}
+
+interface GeneralOptions {
+    val_name?: string
+    re_enter_if_exists?: boolean
+    removeInvalidChars?: boolean
+}
+
+export interface TapOptions extends GeneralOptions {
+    max_digits?: number
+    min_digits?: number
+    sec_wait?: number
+    /**
+     * @default "No"
+     */
+    typing_playback_mode?: 'number' | 'Digits' | 'File' | 'TTS' | 'Alpha' | 'No' | 'HebrewKeyboard' | 'EmailKeyboard' | 'EnglishKeyboard' | 'DigitsKeyboard' | 'TeudatZehut' | 'Price' | 'Time' | 'Phone' | 'No'
+    /**
+     * @default false
+     */
+    block_asterisk_key?: boolean
+    /**
+     * @default false
+     */
+    block_zero_key?: boolean
+    replace_char?: string
+    digits_allowed?: Array<number | string>
+    amount_attempts?: number
+    /**
+     * @default false
+     */
+    allow_empty?: boolean
+    /**
+     * @default "None"
+     */
+    empty_val?: any
+    /**
+     * @default false
+     */
+    block_change_keyboard?: boolean
+}
+
+export interface SstOptions extends GeneralOptions {
+    lang?: string
+    /**
+     * @default false
+     */
+    block_typing?: boolean
+    max_digits?: number
+    use_records_recognition_engine?: boolean
+    quiet_max?: number
+    length_max?: number
+}
+
+export interface RecordOptions extends GeneralOptions {
+    path?: string
+    file_name?: string
+    /**
+     * @default false
+     */
+    no_confirm_menu?: boolean
+    /**
+     * @default true
+     */
+    save_on_hangup?: boolean
+    /**
+     * @default false
+     */
+    append_to_existing_file?: boolean
+    min_length?: number
+    max_length?: number
+}
+
+interface IdListMessageOptions {
+    /**
+     * הסרה אוטומטית של תווים לא חוקיים מתשובות TTS
+     * @default: false
+     */
+    removeInvalidChars?: boolean
+    /**
+     * יש להגדיר במידה ומעוניינים לשרשר פעולות נוספות (לדוגמה read)
+     * @default: false
+     */
+    prependToNextAction?: boolean
+}
+
+export class CallError extends Error {
+    readonly date: Date;
+    readonly call: Call;
+
+    constructor (options: { message: string, call: Call });
+}
+
+export class ExitError extends CallError {
+    readonly context: {
+        caller: 'go_to_folder' | 'id_list_message' | 'routing_yemot'
+        target: string
+    };
+
+    constructor (call: Call, context: ExitError['context']);
+}
+
+export class HangupError extends CallError {
+    constructor (call: Call);
+}
+
+export class TimeoutError extends CallError {
+    readonly timeout: number;
+
+    constructor (call: Call, timeout: number);
+}
